30bd48fec38357c8071558154814e11568f712aa
Noelia Camelia <noeliacamelia@gmail.com>
2025-02-05 22:36:38 +0100

    CardComponent

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import NavbarComponent from "@/components/navbar/NavbarComponent";
 import FooterComponent from "@/components/footer/FooterComponent";
 import Title from "@/components/title/Title";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -23,9 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <NavbarComponent />
         <main>
           <Title />
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,39 @@
-import Image from "next/image";
+import { CardComponent } from "@/components/card/CardComponent";
+import { getAllTreatmentsService } from "@/services/getAllTreatmentsServices";
 
-export default function Home() {
+export default async function Home() {
+  const treatments = await getAllTreatmentsService();
+
   return (
     <>
-      <h2>Treatments</h2>
+      <section>
+        <h3>Tratamientos</h3>
+        <>
+          {!treatments.data.treatments.length ? (
+            <p>Loading</p>
+          ) : (
+            treatments.data.treatments.map((treatment, index) => (
+              <article
+                key={treatment.id}
+                className={`flex flex-col shadow-md mb-5 md:flex-row ${
+                  index % 2 !== 0 ? "md:flex-row-reverse" : ""
+                }`}
+              >
+                <CardComponent
+                  image={treatment.image}
+                  name={treatment.name}
+                  type={treatment.type}
+                  subtype={treatment.subtype}
+                  description={treatment.description}
+                  price={treatment.price}
+                  clarification={treatment.clarification}
+                  id={treatment.id}
+                />
+              </article>
+            ))
+          )}
+        </>
+      </section>
     </>
   );
 }
