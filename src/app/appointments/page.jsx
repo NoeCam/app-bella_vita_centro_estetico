768d601cbe908c85eb4180b753a290fbf34c51ce
Noelia Camelia <noeliacamelia@gmail.com>
2025-02-19 12:21:53 +0100

    within of Suspense-import

--- src/app/appointments/page.jsx
+++ src/app/appointments/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export const PageAppointments = () => {
